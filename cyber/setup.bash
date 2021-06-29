0f394e22e7d462570cce316d9b61edb40c32bacd
Ryan Cheung <zhangxiaoya1989@gmail.com>
2021-06-29 11:13:45 +0800

    [Update] Cyber setup

--- cyber/setup.bash
+++ cyber/setup.bash
@@ -1,48 +1,78 @@
-#! /usr/bin/env bash
-TOP_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd -P)"
-source ${TOP_DIR}/scripts/apollo.bashrc
+# #! /usr/bin/env bash
+# TOP_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd -P)"
+# source ${TOP_DIR}/scripts/apollo.bashrc
 
-export APOLLO_BAZEL_DIST_DIR="${APOLLO_CACHE_DIR}/distdir"
-export CYBER_PATH="${APOLLO_ROOT_DIR}/cyber"
+# export APOLLO_BAZEL_DIST_DIR="${APOLLO_CACHE_DIR}/distdir"
+# export CYBER_PATH="${APOLLO_ROOT_DIR}/cyber"
 
-bazel_bin_path="${APOLLO_ROOT_DIR}/bazel-bin"
-cyber_bin_path="${bazel_bin_path}/cyber"
-cyber_tool_path="${bazel_bin_path}/cyber/tools"
-recorder_path="${cyber_tool_path}/cyber_recorder"
-launch_path="${cyber_tool_path}/cyber_launch"
-channel_path="${cyber_tool_path}/cyber_channel"
-node_path="${cyber_tool_path}/cyber_node"
-service_path="${cyber_tool_path}/cyber_service"
-monitor_path="${cyber_tool_path}/cyber_monitor"
-visualizer_path="${bazel_bin_path}/modules/tools/visualizer"
-rosbag_to_record_path="${bazel_bin_path}/modules/data/tools/rosbag_to_record"
+# bazel_bin_path="${APOLLO_ROOT_DIR}/bazel-bin"
+# cyber_bin_path="${bazel_bin_path}/cyber"
+# cyber_tool_path="${bazel_bin_path}/cyber/tools"
+# recorder_path="${cyber_tool_path}/cyber_recorder"
+# launch_path="${cyber_tool_path}/cyber_launch"
+# channel_path="${cyber_tool_path}/cyber_channel"
+# node_path="${cyber_tool_path}/cyber_node"
+# service_path="${cyber_tool_path}/cyber_service"
+# monitor_path="${cyber_tool_path}/cyber_monitor"
+# visualizer_path="${bazel_bin_path}/modules/tools/visualizer"
+# rosbag_to_record_path="${bazel_bin_path}/modules/data/tools/rosbag_to_record"
 
-# TODO(all): place all these in one place and add_to_path
-for entry in "${cyber_bin_path}" \
-    "${recorder_path}" "${monitor_path}"  \
-    "${channel_path}" "${node_path}" \
-    "${service_path}" \
-    "${launch_path}" \
-    "${visualizer_path}" \
-    "${rosbag_to_record_path}" ; do
-    add_to_path "${entry}"
-done
+# # TODO(all): place all these in one place and add_to_path
+# for entry in "${cyber_bin_path}" \
+#     "${recorder_path}" "${monitor_path}"  \
+#     "${channel_path}" "${node_path}" \
+#     "${service_path}" \
+#     "${launch_path}" \
+#     "${visualizer_path}" \
+#     "${rosbag_to_record_path}" ; do
+#     add_to_path "${entry}"
+# done
 
-# ${CYBER_PATH}/python
-export PYTHONPATH=${bazel_bin_path}/cyber/python/internal:${PYTHONPATH}
+# # ${CYBER_PATH}/python
+# export PYTHONPATH=${bazel_bin_path}/cyber/python/internal:${PYTHONPATH}
+
+# export CYBER_DOMAIN_ID=80
+# export CYBER_IP=127.0.0.1
+
+# export GLOG_log_dir="${APOLLO_ROOT_DIR}/data/log"
+# export GLOG_alsologtostderr=0
+# export GLOG_colorlogtostderr=1
+# export GLOG_minloglevel=0
+
+# export sysmo_start=0
+
+# # for DEBUG log
+# #export GLOG_minloglevel=-1
+# #export GLOG_v=4
+
+# source ${CYBER_PATH}/tools/cyber_tools_auto_complete.bash
+
+
+export CYBER_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+binary_path="$CYBER_PATH/bin"
+lib_path="$CYBER_PATH/lib"
+proto_path="$CYBER_PATH/../../lib"
+
+PYTHON_LD_PATH="/apollo/bazel-bin/cyber/py_wrapper"
+
+qt_path=/usr/local/Qt5.5.1/5.5/gcc_64
+export LD_LIBRARY_PATH=${proto_path}:${lib_path}:$LD_LIBRARY_PATH
+export QT_QPA_PLATFORM_PLUGIN_PATH=${qt_path}/plugins
+export PATH=${binary_path}:${recorder_path}:${monitor_path}:${launch_path}:${channel_path}:${node_path}:${service_path}:${qt_path}/bin:${visualizer_path}:${rosbag_to_record_path}:$PATH
+export PYTHONPATH=${PYTHON_LD_PATH}:${CYBER_PATH}/python:$PYTHONPATH
 
 export CYBER_DOMAIN_ID=80
 export CYBER_IP=127.0.0.1
 
-export GLOG_log_dir="${APOLLO_ROOT_DIR}/data/log"
-export GLOG_alsologtostderr=0
+export GLOG_log_dir=$CYBER_PATH/log
+export GLOG_alsologtostderr=1
 export GLOG_colorlogtostderr=1
 export GLOG_minloglevel=0
 
 export sysmo_start=0
 
 # for DEBUG log
-#export GLOG_minloglevel=-1
-#export GLOG_v=4
+# export GLOG_minloglevel=-1
+export GLOG_v=4
 
-source ${CYBER_PATH}/tools/cyber_tools_auto_complete.bash
+#source ${CYBER_PATH}/tools/cyber_tools_auto_complete.bash
